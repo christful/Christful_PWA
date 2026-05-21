3e828c610c1af62820cf7c0bab9f6d953227b66d
christful <christful7@gmail.com>
2026-05-21 13:34:31 +0000

    feat: add Vercel Analytics integration to layout component for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,6 +56,7 @@ export default function RootLayout({
         // closeButton
         />
         {children}
+        <Analytics />
       </body>
     </html>
   );
